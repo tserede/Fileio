8740536073292b79d92f55ef0ef3a75c6215111a
tserede <kasy.06@mail.ru>
2024-09-11 21:29:05 +0300

    Добавлена функция загрузки данных из файла в формате json и отображения данных в двух списках

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from importlib.metadata import files
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
@@ -40,6 +41,23 @@ def upload():
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
 
+def show_history():
+    if not os.path.exists(history_file):
+        mb.showinfo("История", "История загрузок пуста")
+        return
+    history_window=Toplevel(window)
+    history_window.title("История загрузок")
+
+    files_listbox=Listbox(history_window, width=50, height=20)
+    files_listbox.grid(row=0, column=0, padx=(10,0), pady=10)
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(0, 10), pady=10)
+
+    with open(history_file, 'r') as f:
+        history=json.load(f)
+        for item in history: #history-это список словарей
+            files_listbox.insert(END, item['file_path'])
+            links_listbox.insert(END, item['download_link'])
 window=Tk()
 window.title("Сохранение файлов в облаке")
 window.geometry("400x200")
@@ -48,5 +66,8 @@ button.pack()
 
 entry=ttk.Entry()
 entry.pack()
+
+history_button=ttk.Button(text="Показать историю", command=show_history)
+history_button.pack()
 
 window.mainloop()
